1ea1db3c6093709f2922d034871d112252291bf8
En Yi <en.yi@tutanota.com>
2023-03-13 20:07:52 +0800

    Update the test to use the get functions

--- assets_test.c
+++ assets_test.c
@@ -14,18 +14,22 @@ int main(void)
 
     Texture2D* tex = add_texture(&assets, "testtex", "res/test_tex.png");
 
-    Sprite_t* spr = add_sprite(&assets, "testspr1", tex);
+    add_sprite(&assets, "testspr1", tex);
+    Sprite_t* spr = get_sprite(&assets, "testspr1");
     spr->origin = (Vector2){0, 0};
     spr->frame_size = (Vector2){32, 32};
 
-    Sprite_t* spr2 = add_sprite(&assets, "testspr2", tex);
+    add_sprite(&assets, "testspr2", tex);
+    Sprite_t* spr2 = get_sprite(&assets, "testspr2");
     spr2->frame_count = 4;
     spr2->origin = (Vector2){0, 0};
     spr2->frame_size = (Vector2){32, 32};
     spr2->speed = 15;
 
-    Sound* snd = add_sound(&assets, "testsnd", "res/sound.ogg");
-    Font* fnt = add_font(&assets, "testfont", "res/test_font.ttf");
+    add_sound(&assets, "testsnd", "res/sound.ogg");
+    Sound* snd = get_sound(&assets, "testsnd");
+    add_font(&assets, "testfont", "res/test_font.ttf");
+    Font* fnt = get_font(&assets, "testfont");
 
     while(!WindowShouldClose())
     {

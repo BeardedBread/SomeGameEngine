bafb32501f6d40987ce69faf4f60fd4c0a561e3e
En Yi <en.yi@tutanota.com>
2023-05-13 15:14:05 +0800

    Replace mempools free list data struct
    Changelog:
    - Use circular buffer instead of sc_queue
        - The size is already known beforehand. So circular buffer seems
          to be more appropriate

--- engine/EC/mempool.c
+++ engine/EC/mempool.c
@@ -1,7 +1,8 @@
 #include "mempool.h"
-#include "sc/queue/sc_queue.h"
+//#include "sc/queue/sc_queue.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 // Static allocate buffers
 static Entity_t entity_buffer[MAX_COMP_POOL_SIZE];
@@ -16,12 +17,60 @@ static CHitBoxes_t chitboxes_buffer[MAX_COMP_POOL_SIZE];
 static CHurtbox_t churtbox_buffer[MAX_COMP_POOL_SIZE];
 static CSprite_t csprite_buffer[MAX_COMP_POOL_SIZE];
 
+typedef struct ULongCircBuffer {
+    unsigned long* buffer;     // data buffer
+    unsigned long* buffer_end; // end of data buffer
+    uint32_t capacity;  // maximum number of items in the buffer
+    uint32_t count;     // number of items in the buffer
+    unsigned long* head;       // pointer to head
+    unsigned long* tail;       // pointer to tail
+}ULongCircBuffer_t;
+
+static void cb_init(ULongCircBuffer_t* cb, size_t capacity)
+{
+    cb->buffer = (unsigned long*)malloc(capacity * sizeof(unsigned long));
+    assert(cb->buffer != NULL);
+
+    cb->buffer_end = cb->buffer + capacity;
+    cb->capacity = capacity;
+    cb->count = 0;
+    cb->head = cb->buffer;
+    cb->tail = cb->buffer;
+}
+
+static void cb_free(ULongCircBuffer_t* cb)
+{
+    free(cb->buffer);
+    // clear out other fields too, just to be safe
+}
+
+static bool cb_pop_front(ULongCircBuffer_t* cb, unsigned long* item)
+{
+    if (cb->count == 0) return false;
+
+    *item = *cb->tail;
+    cb->tail++;
+    if(cb->tail == cb->buffer_end) cb->tail = cb->buffer;
+    cb->count--;
+    return true;
+}
+
+static bool cb_push_back(ULongCircBuffer_t* cb, unsigned long item)
+{
+    if(cb->count == cb->capacity) return false;
+    *(cb->head) = item;
+    cb->head++;
+    if(cb->head == cb->buffer_end) cb->head = cb->buffer;
+    cb->count++;
+    return true;
+}
+
 typedef struct MemPool {
     void * const buffer;
     const unsigned long max_size;
     const unsigned long elem_size;
     bool *use_list;
-    struct sc_queue_uint free_list;
+    ULongCircBuffer_t free_list;
 } MemPool_t;
 
 // Static allocate mempools
@@ -55,22 +104,24 @@ void init_memory_pools(void)
             memset(comp_mempools[i].buffer, 0, comp_mempools[i].elem_size * comp_mempools[i].max_size);
             comp_mempools[i].use_list = (bool*)calloc(comp_mempools[i].max_size, sizeof(bool));
             assert(comp_mempools[i].use_list != NULL);
-            sc_queue_init(&comp_mempools[i].free_list);
+            cb_init(&comp_mempools[i].free_list, comp_mempools[i].max_size);
             for (size_t j = 0; j < comp_mempools[i].max_size; ++j)
             {
-                sc_queue_add_last(&comp_mempools[i].free_list, j);
+                comp_mempools[i].free_list.buffer[j] = j;
             }
+            comp_mempools[i].free_list.count = comp_mempools[i].max_size;
         }
 
         memset(ent_mempool.buffer, 0, ent_mempool.elem_size*ent_mempool.max_size);
-        sc_queue_init(&ent_mempool.free_list);
+        cb_init(&ent_mempool.free_list, ent_mempool.max_size);
         ent_mempool.use_list = (bool *)calloc(ent_mempool.max_size, sizeof(bool));
         for (size_t i = 0; i < ent_mempool.max_size; ++i)
         {
             entity_buffer[i].m_id = i;
             sc_map_init_64(&entity_buffer[i].components, 16 ,0);
-            sc_queue_add_last(&(ent_mempool.free_list), i);
+            ent_mempool.free_list.buffer[i] = i;
         }
+        ent_mempool.free_list.count = ent_mempool.max_size;
         pool_inited = true;
     }
 }
@@ -82,10 +133,10 @@ void free_memory_pools(void)
         for (size_t i = 0; i < N_COMPONENTS; ++i)
         {
             free(comp_mempools[i].use_list);
-            sc_queue_term(&comp_mempools[i].free_list);
+            cb_free(&comp_mempools[i].free_list);
         }
         free(ent_mempool.use_list);
-        sc_queue_term(&ent_mempool.free_list);
+        cb_free(&ent_mempool.free_list);
 
         for (int i = 0; i < MAX_COMP_POOL_SIZE; ++i)
         {
@@ -97,8 +148,9 @@ void free_memory_pools(void)
 
 Entity_t* new_entity_from_mempool(unsigned long* e_idx_ptr)
 {
-    if(sc_queue_empty(&ent_mempool.free_list)) return NULL;
-    unsigned long e_idx = sc_queue_del_first(&ent_mempool.free_list);
+    unsigned long e_idx;
+    if (!cb_pop_front(&ent_mempool.free_list, &e_idx)) return NULL;
+
     *e_idx_ptr = e_idx;
     ent_mempool.use_list[e_idx] = true;
     Entity_t* ent = entity_buffer + e_idx;
@@ -119,7 +171,7 @@ void free_entity_to_mempool(unsigned long idx)
     if (ent_mempool.use_list[idx])
     {
         ent_mempool.use_list[idx] = false;
-        sc_queue_add_first(&ent_mempool.free_list, idx);
+        cb_push_back(&ent_mempool.free_list, idx);
     }
 }
 
@@ -127,9 +179,8 @@ void* new_component_from_mempool(ComponentEnum_t comp_type, unsigned long* idx)
 {
     void* comp = NULL;
     assert(comp_type < N_COMPONENTS);
-    if(!sc_queue_empty(&comp_mempools[comp_type].free_list))
+    if (cb_pop_front(&comp_mempools[comp_type].free_list, idx))
     {
-        *idx = sc_queue_del_first(&comp_mempools[comp_type].free_list);
         comp_mempools[comp_type].use_list[*idx] = true;
         comp = comp_mempools[comp_type].buffer + (*idx * comp_mempools[comp_type].elem_size);
         memset(comp, 0, comp_mempools[comp_type].elem_size);
@@ -155,7 +206,7 @@ void free_component_to_mempool(ComponentEnum_t comp_type, unsigned long idx)
     if (comp_mempools[comp_type].use_list[idx])
     {
         comp_mempools[comp_type].use_list[idx] = false;
-        sc_queue_add_last(&comp_mempools[comp_type].free_list, idx);
+        cb_push_back(&comp_mempools[comp_type].free_list, idx);
     }
 }
 
@@ -164,8 +215,8 @@ void print_mempool_stats(char* buffer)
     for (size_t i = 0; i < N_COMPONENTS; ++i)
     {
         buffer += sprintf(
-            buffer, "%lu: %lu/%lu\n",
-            i, sc_queue_size(&comp_mempools[i].free_list), comp_mempools[i].max_size
+            buffer, "%lu: %u/%u\n",
+            i, comp_mempools[i].free_list.count, comp_mempools[i].free_list.capacity
         );
     }
 }
